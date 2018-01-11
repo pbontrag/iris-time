63225525708b571d40164c896e37380d88eca0f7
Matt Bontrager <pbontrag@jaguarlandrover.com>
2018-01-11 11:06:18 -0800

    add tests to iris time server/service

--- config/index.js
+++ config/index.js
@@ -1,7 +1,28 @@
 'use strict';
+
 require('dotenv').config();
+
+const bunyan = require('bunyan');
+
+const log = {
+	development: () => {
+		return bunyan.createLogger({name: 'IRIS-TIME-development', level: 'debug'});
+	},
+	production: () => {
+		return bunyan.createLogger({name: 'IRIS-TIME-production', level: 'info'});
+	},
+	test: () => {
+		return bunyan.createLogger({name: 'IRIS-TIME-test', level: 'fatal'});
+	}
+};
 
 module.exports = {
 	geoToken: process.env.GEO_TOKEN,
-	timeToken: process.env.TIME_TOKEN
+	timeToken: process.env.TIME_TOKEN,
+	log: (env) => {
+		if (env) {
+			return log[env]();
+		}
+		return log[process.env.NODE_ENV || 'development']();
+	}
 };
--- server/service.js
+++ server/service.js
@@ -1,36 +1,35 @@
 'use strict';
-const config = require('../config');
+
 const express = require('express');
 const service = express();
 const request = require('superagent');
 const moment = require('moment');
 
+module.exports = (config) => {
+	const log = config.log();
+	service.get('/service/:location', (req, res) => {
+		request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + config.geoToken, (err, response) => {
+			if (err) {
+				log.error(err);
+				return res.sendStatus(500);
+			}
 
-service.get('/service/:location', (req, res, next) => {
+			const location = response.body.results[0].geometry.location;
+			const timestamp = +moment().format('X');
 
-    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + config.geoToken, (err, response) => {
-        if(err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
+			request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + config.timeToken, (err, response) => {
+				if (err) {
+					log.error(err);
+					return res.sendStatus(500);
+				}
 
-        const location = response.body.results[0].geometry.location;
-        const timestamp = +moment().format('X');
+				const result = response.body;
 
-        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + config.timeToken, (err, response) => {
-            if(err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
+				const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
 
-            const result = response.body;
-
-            const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
-
-            res.json({result: timeString});
-        });
-    });
-
-});
-
-module.exports = service;
+				res.json({result: timeString});
+			});
+		});
+	});
+	return service;
+};
